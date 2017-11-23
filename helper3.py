sign fixed in k plus int

helper3.py
@@ -73,6 +73,9 @@ k_plusV = np.vectorize(k_plus)
 
 #the lambda expression
 def int_k_plus_sum(s1, s2, d, bta, k, k_on, n_neighbours, w=False):
+    #this is andatory because direction of integration does not matter
+    if s2 < s1: s1, s2 = s2, s1
+    
     s1t = s1 + d/2
     s1tw = wrapping(s1t, d)
     s2t = s2 + d/2
@@ -94,12 +97,12 @@ def int_k_plus_sum(s1, s2, d, bta, k, k_on, n_neighbours, w=False):
         for n in [i - n_neighbours for i in range(2 * n_neighbours + 1)]:
             
             #AAAACHtung: n oder 0 in lambda???
-            res += abs(integralt(s2tw, n) - integralt(s1tw, n)) + (abs((s2t - s1t)) // d) * int_full_period(n) #// is floor division in python
+            res += integralt(s2tw, n) - integralt(s1tw, n) + ((s2t - s1t) // d) * int_full_period(n) #// is floor division in python
         return res
     else:
         res = 0
         for n in [i - n_neighbours for i in range(2 * n_neighbours + 1)]:
-           res += abs(integralt(s2tw, n) - integralt(s1tw, n)) + (abs((s2t - s1t)) // d + 1) * int_full_period(n) #// is floor division in python
+           res += integralt(s2tw, n) - integralt(s1tw, n) + ((s2t - s1t) // d + 1) * int_full_period(n) #// is floor division in python
         return res
 
 #attaching rate sum, either feed already wrapped s in or set w=True!