correct use of average_norm_force: in the end of loop in simulation3.py

simulation3.py
@@ -642,7 +642,7 @@ class simulation:
             sum_P_w = self.sum_P[run][np.newaxis]
             sum_P_w = sum_P_w.T
 
-
+            #averge out the data before equilibrium
             if self.option[run] in ['const', 'poly'] and self.mode[run] == 'vControl':
                 self.average_norm_force_single(run)
             if self.mode[run] == 'fControl':